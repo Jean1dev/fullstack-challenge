435301c0484964dcea15426e4e8d334d3d9f9418
Willian Costa <willian95@outlook.com>
2024-06-23 14:37:13 -0300

    feat: adição de entidade dates

---
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/domain/entities/themoviedb/Dates.java
@@ -0,0 +1,30 @@
+package com.willian.AlpacaFilmes.domain.entities.themoviedb;
+
+public class Dates {
+    private String maximum;
+    private String minimum;
+
+    public Dates() {
+    }
+
+    public Dates(String maximum, String minimum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public String getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(String maximum) {
+        this.maximum = maximum;
+    }
+
+    public String getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(String minimum) {
+        this.minimum = minimum;
+    }
+}
