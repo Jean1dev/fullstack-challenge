fd88983b7f0eea1d819050a393fd93445571a09f
Willian Costa <willian95@outlook.com>
2024-06-23 14:22:29 -0300

    feat: adição de configuração para a openApi(Swagger)

---
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.willian.AlpacaFilmes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Order Swagger OpenAPI").version("V1")
+                .description("Some Description about yout API.").termsOfService("https://pub.willian.com.br/API")
+                .license(new License().name("Apache 2.0").url("https://pub.willian.com.br/API")));
+    }
+}
