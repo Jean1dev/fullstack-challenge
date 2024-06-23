4d0c2943419f4aab9775e99bd6c5a642b8292f90
Willian Costa <willian95@outlook.com>
2024-06-23 14:08:24 -0300

    build: configurações inciais do projeto

---
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/AlpacaFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.willian.AlpacaFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacaFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpacaFilmesApplication.class, args);
+	}
+
+}
---
+++ willian/AlpacaFilmes/src/test/java/com/willian/AlpacaFilmes/AlpacaFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.AlpacaFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpacaFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
