5c60425782938e0faed0d93d0ebf5629a46569e4
Willian Costa <willian95@outlook.com>
2024-06-23 14:15:38 -0300

    feat: adição de DTO para entidade cinema

---
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/domain/dto/CinemaDTO.java
@@ -0,0 +1,49 @@
+package com.willian.AlpacaFilmes.domain.dto;
+
+import com.willian.AlpacaFilmes.domain.entities.Cinema;
+
+import java.io.Serializable;
+
+public class CinemaDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String nome;
+
+    public CinemaDTO() {
+    }
+
+    public CinemaDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public CinemaDTO(Cinema cinema) {
+        this.id = cinema.getId();
+        this.nome = cinema.getNome();
+    }
+
+    public static Cinema converter(CinemaDTO cinemaDTO) {
+        Cinema cinema = new Cinema();
+        cinema.setId(cinemaDTO.getId());
+        cinema.setNome(cinemaDTO.getNome());
+        return cinema;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
