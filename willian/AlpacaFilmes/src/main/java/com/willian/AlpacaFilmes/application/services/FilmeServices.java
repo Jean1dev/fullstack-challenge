ff807e3a045526d5541a4c1142f20e54b53bc12f
Willian Costa <willian95@outlook.com>
2024-06-24 11:52:00 -0300

    feat: adição de tratamento de exceptions

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/services/FilmeServices.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/services/FilmeServices.java
@@ -1,9 +1,12 @@
 package com.willian.AlpacaFilmes.application.services;
 
+import com.willian.AlpacaFilmes.application.exceptions.ApiResponseException;
 import com.willian.AlpacaFilmes.domain.entities.Filme;
 import com.willian.AlpacaFilmes.infra.repositories.FilmesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +15,8 @@ import java.util.logging.Logger;
 @Service
 public class FilmeServices {
     Logger logger = Logger.getLogger(TheMovieDbServices.class.getName());
+    private static final int MAX_RETRIES = 3;
+    private static final int RETRY_DELAY_MS = 60000;
 
     @Autowired
     private FilmesRepository filmesRepository;
@@ -19,17 +24,39 @@ public class FilmeServices {
     @Autowired
     private TheMovieDbServices movieDbServices;
 
+    @Transactional
     public void salvarFilmes() {
-        List<Filme> filmes = movieDbServices.getMovies();
+        int attempts = 0;
+        while(attempts < MAX_RETRIES) {
+            try {
+                List<Filme> filmes = movieDbServices.getMovies();
 
-        for (Filme filme : filmes) {
-            Optional<Filme> estaRegistrado = filmesRepository.findById(filme.getId());
+                for (Filme filme : filmes) {
+                    Optional<Filme> estaRegistrado = filmesRepository.findById(filme.getId());
 
-            if (estaRegistrado.isEmpty()) {
-                filmesRepository.save(filme);
+                    if (estaRegistrado.isEmpty()) {
+                        filmesRepository.save(filme);
+                    }
+                }
+                logger.info("Filmes Atualizados");
+
+            } catch (ApiResponseException ae) {
+                attempts++;
+                logger.warning("Falha ao resgatar filmes. Tentativa " + attempts + " de " + MAX_RETRIES);
+
+                if (attempts >= MAX_RETRIES) {
+                    logger.warning("Número máximo de tentativas atingido. Abortando." + ae.getMessage());
+                }
+
+                try {
+                    Thread.sleep(RETRY_DELAY_MS);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    logger.warning("Thread interrompida" + ie);
+                }
+            } catch (Exception ex) {
+                logger.warning("Erro inesperado ao tentar salvar filmes: " + ex.getMessage());
             }
         }
-
-        logger.info("Filmes Atualizados");
     }
 }
