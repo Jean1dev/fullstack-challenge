12a11643a8b5074e7a159920ef483f85c14d0b22
Willian Costa <willian95@outlook.com>
2024-06-25 15:45:22 -0300

    refactor: espaços em update status

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/services/CadeirasServices.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/services/CadeirasServices.java
@@ -1,14 +1,12 @@
 package com.willian.AlpacaFilmes.application.services;
 
 import com.willian.AlpacaFilmes.application.exceptions.ResourceNotFoundException;
-import com.willian.AlpacaFilmes.domain.dto.CadeirasDTO;
 import com.willian.AlpacaFilmes.domain.entities.Cadeiras;
 import com.willian.AlpacaFilmes.domain.enums.CadeiraStatus;
 import com.willian.AlpacaFilmes.infra.repositories.CadeirasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class CadeirasServices {
@@ -19,7 +17,7 @@ public class CadeirasServices {
         if(status == null) throw new IllegalArgumentException("Status não pode ser nulo!");
 
         Cadeiras cadeiras = cadeirasRepository.findById(id).orElseThrow(() ->
-                new ResourceNotFoundException("Nenhum dado encontrado para o Id" + id + "!"));
+                new ResourceNotFoundException("Nenhum dado encontrado para o Id " + id + "!"));
 
         cadeiras.setStatus(status);
 
