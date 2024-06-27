16cd50c91e5e3f3f6424461d19e99939cb0f581d
Willian Costa <willian95@outlook.com>
2024-06-27 14:28:10 -0300

    feat: adição de configurações para os testes de integrações

---
+++ willian/AlpacaFilmes/src/test/java/com/willian/AlpacaFilmes/integrationsTest/configs/TestConfigs.java
@@ -0,0 +1,10 @@
+package com.willian.AlpacaFilmes.integrationsTest.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+
+    public static  final String CONTENT_TYPE_JSON = "application/json";
+}
