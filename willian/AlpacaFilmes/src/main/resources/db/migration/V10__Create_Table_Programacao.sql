c7443bf6afeaf2210fdb01803820fe8864e9d78e
Willian Costa <willian95@outlook.com>
2024-07-03 15:27:44 -0300

    fix: migration para a tabela programação

--- willian/AlpacaFilmes/src/main/resources/db/migration/V10__Create_Table_Programacao.sql
+++ willian/AlpacaFilmes/src/main/resources/db/migration/V10__Create_Table_Programacao.sql
@@ -1,11 +1,9 @@
 CREATE TABLE `programacao` (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    id_filme bigint,
-    id_sala bigint,
-    id_horario bigint,
-    dataCadastro bigint,
-    FOREIGN KEY (id_filme) REFERENCES filmes(id),
-    FOREIGN KEY (id_sala) REFERENCES Salas(id),
-    FOREIGN KEY (id_horario) REFERENCES Horarios(id),
+    `id_filme` bigint,
+    `id_sala` bigint,
+    `data_cadastro` datetime(6),
+    FOREIGN KEY (id_filme) REFERENCES `filmes`(`id`),
+    FOREIGN KEY (id_sala) REFERENCES `salas`(`id`),
     PRIMARY KEY (`id`)
 );
