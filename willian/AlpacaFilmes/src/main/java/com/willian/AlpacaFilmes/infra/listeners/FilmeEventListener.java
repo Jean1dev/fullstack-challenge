4435b98b20795605d61592c2feaa871783418cb8
Willian Costa <willian95@outlook.com>
2024-07-03 10:27:29 -0300

    fix: correção do serviço injetado no filme event listener

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/listeners/FilmeEventListener.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/listeners/FilmeEventListener.java
@@ -1,6 +1,6 @@
 package com.willian.AlpacaFilmes.infra.listeners;
 
-import com.willian.AlpacaFilmes.application.services.FilmeServices;
+import com.willian.AlpacaFilmes.application.services.ProgamacaoServices;
 import com.willian.AlpacaFilmes.common.events.FilmeEvent;
 import com.willian.AlpacaFilmes.domain.entities.Filme;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class FilmeEventListener {
     @Autowired
-    private FilmeServices filmeServices;
+    private ProgamacaoServices progamacaoServices;
 
     @EventListener
     public void handleFilmeEvent(FilmeEvent event) {
         Filme filme = event.getFilme();
-        filmeServices.salvarFilmes();
+        progamacaoServices.criarProgramacao(filme);
     }
 }
