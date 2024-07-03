72045c212feca025d2e15d568fa3ecbf9e4fdfeb
Willian Costa <willian95@outlook.com>
2024-07-03 15:16:04 -0300

    refactor: Filme event

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/common/events/FilmeEvent.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/common/events/FilmeEvent.java
@@ -1,13 +1,11 @@
 package com.willian.AlpacaFilmes.common.events;
 
 import com.willian.AlpacaFilmes.domain.entities.Filme;
-import org.springframework.context.ApplicationEvent;
 
-public class FilmeEvent extends ApplicationEvent {
+public class FilmeEvent {
     private final Filme filme;
 
-    public FilmeEvent(Object source, Filme filme) {
-        super(source);
+    public FilmeEvent(Filme filme) {
         this.filme = filme;
     }
 
