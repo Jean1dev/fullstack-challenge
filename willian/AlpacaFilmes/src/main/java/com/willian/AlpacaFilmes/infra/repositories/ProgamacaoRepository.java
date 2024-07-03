6446041fd670f3f4cf07a34a1e333a3c32ff9a32
Willian Costa <willian95@outlook.com>
2024-07-03 15:15:10 -0300

    feat: adição de metodo findTop4ByOrderByIdDesc em Programacao repository

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/repositories/ProgamacaoRepository.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/repositories/ProgamacaoRepository.java
@@ -4,6 +4,10 @@ import com.willian.AlpacaFilmes.domain.entities.Programacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProgamacaoRepository extends JpaRepository<Programacao, Long> {
+
+    List<Programacao> findTop4ByOrderByIdDesc();
 }
