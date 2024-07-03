3ab32923e6d10aefce4725e203d5ca117a3cabd0
Willian Costa <willian95@outlook.com>
2024-07-03 15:15:46 -0300

    refactor: Filme event listener

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/listeners/FilmeEventListener.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/infra/listeners/FilmeEventListener.java
@@ -1,20 +1,34 @@
 package com.willian.AlpacaFilmes.infra.listeners;
 
-import com.willian.AlpacaFilmes.application.services.ProgamacaoServices;
+import com.willian.AlpacaFilmes.application.services.CriarProgamacaoServices;
+import com.willian.AlpacaFilmes.application.services.TheMovieDbServices;
 import com.willian.AlpacaFilmes.common.events.FilmeEvent;
 import com.willian.AlpacaFilmes.domain.entities.Filme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.logging.Logger;
 
 @Component
 public class FilmeEventListener {
     @Autowired
-    private ProgamacaoServices progamacaoServices;
+    private CriarProgamacaoServices progamacaoServices;
+
+    Logger logger = Logger.getLogger(TheMovieDbServices.class.getName());
 
     @EventListener
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleFilmeEvent(FilmeEvent event) {
         Filme filme = event.getFilme();
-        progamacaoServices.criarProgramacao(filme);
+
+        try {
+            progamacaoServices.criarProgramacao(filme);
+        } catch (Exception e) {
+            logger.warning("Conflito ao criar programação para o filme: " + filme.getId());
+        }
     }
+
 }
