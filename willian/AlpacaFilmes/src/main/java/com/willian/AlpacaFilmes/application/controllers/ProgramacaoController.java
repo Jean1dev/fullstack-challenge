368f594429467613dd9eb6e997594ca777d0bafe
Willian Costa <willian95@outlook.com>
2024-07-10 17:16:41 -0300

    feat: adição de media type nas anotations do programação controller

--- willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/controllers/ProgramacaoController.java
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/application/controllers/ProgramacaoController.java
@@ -4,6 +4,7 @@ import com.willian.AlpacaFilmes.application.controllers.interfaces.IProgramacaoC
 import com.willian.AlpacaFilmes.application.services.ProgramacaoServices;
 import com.willian.AlpacaFilmes.domain.dto.ProgramacaoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,21 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "v1/programacao")
+@RequestMapping("v1/programacao")
 public class ProgramacaoController implements IProgramacaoController {
 
     @Autowired
     private ProgramacaoServices programacaoServices;
 
     @Override
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ProgramacaoDTO>> pegarTodos() {
         List<ProgramacaoDTO> programacaoDTOList = programacaoServices.pegarTodos();
         return ResponseEntity.ok().body(programacaoDTOList);
     }
 
     @Override
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProgramacaoDTO> pegarPorId(@PathVariable(value = "id") Long id) {
         ProgramacaoDTO programacaoDTO = programacaoServices.pegarPorId(id);
         return ResponseEntity.ok().body(programacaoDTO);
