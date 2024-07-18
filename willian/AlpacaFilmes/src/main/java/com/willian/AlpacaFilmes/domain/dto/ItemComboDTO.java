481be371bb871477e5220c458e3df3d8ad1644e2
Willian Costa <willian95@outlook.com>
2024-07-18 11:57:57 -0300

    feat: adição de Item Combo DTO

---
+++ willian/AlpacaFilmes/src/main/java/com/willian/AlpacaFilmes/domain/dto/ItemComboDTO.java
@@ -0,0 +1,43 @@
+package com.willian.AlpacaFilmes.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.willian.AlpacaFilmes.domain.entities.Item;
+
+import java.io.Serializable;
+
+@JsonRootName("lista_items")
+public class ItemComboDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+
+    private String nome;
+
+    public ItemComboDTO() {
+    }
+
+    public ItemComboDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public ItemComboDTO(Item item) {
+        this.id = item.getId();
+        this.nome = item.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
