27f8c77a5d28eb3780c2f988b8f819c4f21fefcb
Willian Costa <willian95@outlook.com>
2024-07-19 15:34:41 -0300

    feat: adição de migrations para tablea tipo ingresso

---
+++ willian/AlpacaFilmes/src/main/resources/db/migration/V18__Create_Table_TipoIngresso.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `tipo_ingresso` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `tipo_ingresso` VARCHAR(10),
+    `preco` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`)
+);
---
+++ willian/AlpacaFilmes/src/main/resources/db/migration/V19__Populate_Table_TipoIngresso.sql
@@ -0,0 +1,3 @@
+INSERT INTO `tipo_ingresso` (`tipo_ingresso`, `preco`)
+VALUES ("MEIA", 20.0),
+("INTEIRA", 20.0);
