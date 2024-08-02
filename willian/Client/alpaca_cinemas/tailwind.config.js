6407880a2cdbff93b40c685b9f60095b1d992c7f
WillianCosta <willian95@outlook.com>
2024-08-02 18:36:06 -0300

    chore: adição de cores base do projeto nas configurações do tailwind

--- willian/Client/alpaca_cinemas/tailwind.config.js
+++ willian/Client/alpaca_cinemas/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "custom-blue": "#93B3F1",
+        "custom-gray": "#5C8C8C",
+        "custom-dark-blue": "#2D3A4E",
+        "custom-red": "#AE5959",
+        "custom-light-blue": "#3285D2"
+      }
+    }
   },
   plugins: [require("daisyui")]
 };
