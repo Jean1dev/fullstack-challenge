1b900f09a1405418092ce3d287d91a9576474bbb
WillianCosta <willian95@outlook.com>
2024-08-02 19:45:03 -0300

    feat: adição de utils para converter a data

---
+++ willian/Client/alpaca_cinemas/src/common/utils/DataConverter.ts
@@ -0,0 +1,22 @@
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro"
+];
+
+export const formatDateToBrazilian = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} de ${month} de ${year}`;
+};
