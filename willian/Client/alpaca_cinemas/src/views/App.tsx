e92a528689142d02bf4e153edafeadceb2ffc755
WillianCosta <willian95@outlook.com>
2024-08-02 17:54:27 -0300

    build: criação do client

---
+++ willian/Client/alpaca_cinemas/src/views/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.module.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Alpaca Cinemas</p>
+    </div>
+  );
+};
+
+export default App;
