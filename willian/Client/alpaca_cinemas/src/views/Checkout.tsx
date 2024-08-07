5e75b4fab23d06359be67426d68f84253a51bc55
WillianCosta <willian95@outlook.com>
2024-08-07 18:55:20 -0300

    feat: adição de tela de checkout

--- willian/Client/alpaca_cinemas/src/views/Checkout.tsx
+++ willian/Client/alpaca_cinemas/src/views/Checkout.tsx
@@ -1,50 +1,102 @@
 import React, { useState } from "react";
-import RadioGroup from "../components/RadioGroup";
-import Select from "../components/Select";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { Cadeira } from "../common/types/Cadeira";
+import CadeiraLivre from "../assets/cadeira_livre.svg";
+import CadeiraSelecionada from "../assets/cadeira_selecionada.svg";
+import CadeiraOcupada from "../assets/cadeira_ocupada.svg";
+import CheckoutOptions from "../components/ChekoutOptions";
 
 const Checkout = () => {
   const [selectedRadio, setSelectedRadio] = useState("seg");
   const [tipoIngresso, setTipoIngresso] = useState("");
 
+  const programacoes = useSelector(
+    (state: RootState) => state.programacao.programacao
+  );
+
   const radioOptions = [
-    { label: "Seg", value: "seg", day: 19 },
-    { label: "Ter", value: "ter", day: 20 },
-    { label: "Qua", value: "qua", day: 21 },
-    { label: "Qui", value: "qui", day: 22 },
-    { label: "Sex", value: "sex", day: 23 }
+    { label: "13h00", value: "seg" },
+    { label: "15h00", value: "ter" },
+    { label: "18h00", value: "qua" },
+    { label: "20h00", value: "qui" }
   ];
   const selectTipoIngresso = [
     { label: "Ingresso inteira", value: "INTEIRA" },
     { label: "Ingresso Meia entrada", value: "MEIA" }
   ];
 
+  const cadeiras: Cadeira[] = [
+    { id: 1, numero: 1, status: "livre" },
+    { id: 2, numero: 1, status: "Ocupada" },
+    { id: 3, numero: 1, status: "livre" },
+    { id: 4, numero: 1, status: "livre" },
+    { id: 5, numero: 1, status: "selecionada" },
+    { id: 6, numero: 1, status: "livre" },
+    { id: 7, numero: 1, status: "livre" },
+    { id: 8, numero: 1, status: "livre" },
+    { id: 9, numero: 1, status: "livre" },
+    { id: 10, numero: 1, status: "livre" }
+  ];
+
+  const chunkArray = (arr: any[], size: number) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  };
+
+  const rows = chunkArray(cadeiras, 3);
+
   return (
     <div className="hero bg-custom-dark-blue min-h-screen">
-      <div className="hero-content text-center">
-        <div className="max-w-md w-full p-4 bg-custom-gray rounded-lg flex flex-wrap gap-6 md:gap-6">
-          {/* <div className="w-full md:w-auto flex gap-2 md:gap-2 text-sm md:text-base min-w-[150px]">
-            Data:
-            <RadioGroup
-              options={radioOptions}
-              selectedValue={selectedRadio}
-              onRadioChange={setSelectedRadio}
-            />
-          </div> */}
-          <div className="w-full md:w-auto flex gap-2 md:gap-2 text-sm md:text-base min-w-[150px]">
-            <Select
-              options={selectTipoIngresso}
-              label={"Selecione o horário:"}
-              onChange={setTipoIngresso}
-              value={selectTipoIngresso[0].value}
-            />
-          </div>
-          <div className="w-full md:w-auto flex gap-2 md:gap-2 text-sm md:text-base min-w-[150px]">
-            <Select
-              options={selectTipoIngresso}
-              label={"Tipo de ingresso:"}
-              onChange={setTipoIngresso}
-              value={selectTipoIngresso[0].value}
-            />
+      <div className="">
+        <CheckoutOptions
+          radioOptions={radioOptions}
+          selectedRadio={selectedRadio}
+          setSelectedRadio={setSelectedRadio}
+          selectTipoIngresso={selectTipoIngresso}
+          setTipoIngresso={setTipoIngresso}
+        />
+        <div className="hero bg-custom-dark-blue min-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <div className="max-w-sm">
+              <p>Filme:</p>
+              <h1 className="text-2xl font-bold">
+                {programacoes![0].filme.title}
+              </h1>
+              <p>Cadeiras:</p>
+              <h1 className="text-2xl font-bold">01, 02, 03</h1>
+              <p>Horario:</p>
+              <h1 className="text-2xl font-bold">16H00</h1>
+            </div>
+
+            <div>
+              {rows.map((row, rowIndex) => (
+                <div key={rowIndex} className="flex justify-center mb-4">
+                  {row.map((cadeira) => {
+                    let src;
+                    if (cadeira.status === "Ocupada") {
+                      src = CadeiraOcupada;
+                    } else if (cadeira.status === "selecionada") {
+                      src = CadeiraSelecionada;
+                    } else {
+                      src = CadeiraLivre;
+                    }
+
+                    return (
+                      <img
+                        key={cadeira.id}
+                        src={src}
+                        alt="cadeira"
+                        className="mx-2"
+                      />
+                    );
+                  })}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
