29a5f19505f26d45aafcd1c9969bc2dba8115bdc
WillianCosta <willian95@outlook.com>
2024-08-07 22:57:14 -0300

    feat: adição criar ingreso type

---
+++ willian/Client/alpaca_cinemas/src/common/types/CriarIngresso.ts
@@ -0,0 +1,9 @@
+export interface CriarIngresso {
+    comboList: number[],
+    programacao: number,
+    horario: number,
+    tipoIngresso: number,
+    cadeira: number,
+    nome: string
+    documento: string;
+}
